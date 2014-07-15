fb756ecd5aff33405c11aada631aa78717e95d4f
Josh Dingus <josh.dingus@gmail.com>
2014-07-15 11:43:40 +0000

    dbase connected

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 import csv
+import psycopg2
+
+def db_connect():
+  """
+  Connect to dbase and report if not connected
+  """
+  try:
+      conn = psycopg2.connect("dbname='pets' host='localhost' password='dbpass'")
+      print "Connect to dbase!"
+      return conn
+  except:
+      print "I am unable to connect to the database"
+      quit()
 
 def load_dict(csv_file):
 	"""
@@ -32,12 +45,24 @@ def insert_nulls(csv_list_dict):
 			if item[key] == '':
 				item[key] = 'NULL'
 
+def in_dbase(cursor_obj):
+  pass
+  conn = cursor_obj
+  cursor = conn.cursor()
+#   cursor.execute('SELECT COUNT(*) FROM species WHERE species.name = Cat')  
+#     cursor.execute('SELECT * FROM species')  
+
 
 def main():
-	""" Main function """
-	csv_list = load_dict('pets_to_add.csv')
-
-	insert_nulls(csv_list)
+  """ Main function """
+  conn = db_connect()
+  csv_list = load_dict('pets_to_add.csv') # Create list of dictionaries as csv_header[data]
+  insert_nulls(csv_list)  # Checks all blank entries in CSV and inserts NULL
+  
+  """ Check to see if entries in dbase if not add, normalize for caps """
+  in_dbase(conn) # conn,'species','dog'
+#   in_dbase(conn,'breed')
+#   in_dbase(conn,'shelter')
 
 
 if __name__ == "__main__":
